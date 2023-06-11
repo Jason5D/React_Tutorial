6287221d5637df21d8a79883ad204f620483b7ee
Jason <jason5donoghue@gmail.com>
2023-06-11 12:13:14 +0100

    basic board built

---
+++ reacttutorial/src/App.js
@@ -0,0 +1,17 @@
+import Board from './components/Board'
+import './styles.css'
+
+
+function App() {
+
+
+
+  return (
+    <div className="App">
+      <p>Hello World</p>
+      <Board />
+    </div>
+  );
+}
+
+export default App;
